af86a4023c081c9c8d39265f8c9b3ebcb963c7a8
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-08-20 10:22:30 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -3,7 +3,7 @@ import Device
 device = Device.Particle('340055000f51353532343635', '034a7e5d14e8d1af0be253f67c69a28e1312e262')
 
 def test():
-    device.ota("/home/vagrant/spanner/BuildFactory/code/repositories/mike/b_AAyeIJlNz5BVEKLNSWjJ1rw3/mike_binary.bin")
+    device.ota("binary/mike_binary.bin")
 
 if __name__ == "__main__":
     test()
