a6672061bc63b9edf43f59184dbe180337bd7b68
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-08-29 11:13:05 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -3,7 +3,7 @@ import Device
 device = Device.Particle('340055000f51353532343635', '034a7e5d14e8d1af0be253f67c69a28e1312e262')
 
 def test():
-    device.ota("binary/mike_binary.bin")
+    device.digitalRead("D0")
 
 if __name__ == "__main__":
     test()
