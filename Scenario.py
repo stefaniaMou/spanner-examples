e2d66e2810fe7b8f6fb501989d207a17f21fc257
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-09-18 13:13:06 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -2,7 +2,7 @@ import time
 from Spanner import Spanner
 from Testboard import Testboard
 
-testboard = Testboard('Tester2')
+testboard = Testboard('Tester1')
 
 # Our device's 1st Analog Output Pin will be connected to the Testboard's A0, making it our Input Pin 1
 INPUT_PIN_1 = "A0"
