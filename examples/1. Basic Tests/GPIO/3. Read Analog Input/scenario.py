63fc0a6841b4ae11589a2f9e9fb0750c480f7d96
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-09-18 12:10:48 +0300

    Update scenario.py

--- examples/1. Basic Tests/GPIO/3. Read Analog Input/scenario.py
+++ examples/1. Basic Tests/GPIO/3. Read Analog Input/scenario.py
@@ -17,10 +17,7 @@ import time
 from Spanner import Spanner
 from Testboard import Testboard
 
-
-
-TESTBOARD_ID = "200023001347343438323536"
-testboard = Testboard(TESTBOARD_ID)
+testboard = Testboard("testboard_name")
 
 # Our device's 1st Analog Output Pin will be connected to the Testboard's A0,
 # making it our Input Pin 1
