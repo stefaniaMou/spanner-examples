2b961f41c447df854cb0934e9b9d991d76e89d3e
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-09-18 17:10:38 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -29,7 +29,7 @@ def measure_power_consumption():
     testboard.startPowerMeasurement()
 
     # Measure for 5 minutes
-    time.sleep(5*60)
+    time.sleep(1*60)
 
     # Stop measuring power consumption
     testboard.stopPowerMeasurement()
@@ -38,7 +38,7 @@ def measure_power_consumption():
     # measuredPowerConsumption() will return the total power consumption
     # measured in the measuring period, in mAh. Then we use the assertLessThan()
     # function to assert that this is less than the target value of 100.
-    spanner.assertLessThan(100, testboard.measuredPowerConsumption())
+    spanner.assertLessThan(10, testboard.measuredPowerConsumption())
 
 if __name__ == "__main__":
     measure_power_consumption()
